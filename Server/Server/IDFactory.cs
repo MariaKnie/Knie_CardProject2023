3b5c3864a70e4f5f9c7a096a0dabc586abadd4bb
MariaKnie <mariaknie@gmx.at>
2023-12-25 15:39:08 +0100

    User Registration

---
+++ Server/Server/IDFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Server
+{
+    internal class IDFactory
+    {
+        public string generateID()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
+
+        public string generateID(string sourceUrl)
+        {
+            return string.Format("{0}_{1:N}", sourceUrl, Guid.NewGuid());
+        }
+    }
+}
--- Server/Server/Requests/UserRequests.cs
+++ Server/Server/Requests/UserRequests.cs
@@ -6,25 +6,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.Json;
+using System.Data;
+using Npgsql;
 
 namespace Server.Server.UserRequests
 {
     internal class UserRequests
     {
 
-        public async Task UserRequest(StreamWriter writer, string requesttype, Dictionary<string, string> userInfo )
+        public async Task UserRequest(StreamWriter writer, string requesttype, Dictionary<string, string> userInfo)
         {
             HTTP_Response response = new HTTP_Response();
             string description = $"General User {requesttype}";
             string responseHTML = "";
-            string fullinfo =  userInfo?["body"];
+            string fullinfo = userInfo?["body"];
 
             Console.WriteLine(fullinfo);
-            UserEndpoint m = JsonSerializer.Deserialize<UserEndpoint>(fullinfo);
+            UserEndpoint userToEndpoint = JsonSerializer.Deserialize<UserEndpoint>(fullinfo);
 
             responseHTML += "<html> <body> \n";
             responseHTML += $"<h1> {requesttype} Registered User </h1>";
-            responseHTML += $"\n Username: {m.Username} Password: {m.Password}";
+            responseHTML += $"\n Username: {userToEndpoint.Username} Password: {userToEndpoint.Password}";
+            responseHTML += "\n FullBody: " + userInfo?["body"];
 
 
             if (requesttype == "GET")
@@ -32,12 +35,24 @@ namespace Server.Server.UserRequests
             }
             else if (requesttype == "POST")
             {
+                if (SeeIfUserIsINDB(userToEndpoint.Username) == false) // if user isnt already registered
+                {
+                    responseHTML += "\n Username is AVAILABLE";
+                    responseHTML += "\n Rgistration continue!";
+
+                    AddUsertoDb(userToEndpoint);
+
+
+                }
+                else
+                {
+                    responseHTML += "\n Username is UNAVAILABLE";
+                }
             }
             else if (requesttype == "DEL")
             {
             }
 
-            responseHTML += "\n FullBody: " + userInfo?["body"];
             responseHTML += "\n</body> </html>";
             response.UniqueResponse(writer, 200, description, responseHTML);
         }
@@ -69,8 +84,81 @@ namespace Server.Server.UserRequests
 
             responseHTML += "\n FullBody: " + userInfo?["subpath"];
             responseHTML += "\n</body> </html>";
-            response.UniqueResponse( writer, 200, description, responseHTML);
+            response.UniqueResponse(writer, 200, description, responseHTML);
 
         }
+
+
+
+        static void AddParameterWithValue(IDbCommand command, string paramName, DbType dbType, object value)
+        {
+            command.Parameters.Add(new NpgsqlParameter(paramName, dbType) { Value = value });
+        }
+        public bool SeeIfUserIsINDB(string username)
+        {
+            // Connection
+            var connString = "Host=localhost; Username=postgres; Password=postgres; Database=mydb";
+            using IDbConnection connection = new NpgsqlConnection(connString);
+            connection.Open();
+
+            // command
+            using IDbCommand command = connection.CreateCommand();
+            string query = "SELECT COUNT(*) FROM users WHERE Username = @Username";
+            command.CommandText = query;
+
+            // parameters
+            //command.Parameters.AddWithValue("@Username", username);
+            AddParameterWithValue(command, "@Username", DbType.String, username);
+
+
+            var count = command.ExecuteScalar();
+
+            if (count == null)
+            {
+                Console.WriteLine("AN ERROR OCCURED");
+                return true;
+            }
+
+
+            if ((Int64)count > 0)
+            {
+                Console.WriteLine("Username exists in the database.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Username does not exist in the database.");
+                return false;
+            }
+
+        }
+
+
+        public void AddUsertoDb(UserEndpoint user)
+        {
+            // Connection
+            var connString = "Host=localhost; Username=postgres; Password=postgres; Database=mydb";
+            using IDbConnection connection = new NpgsqlConnection(connString);
+            connection.Open();
+
+            // command
+            using IDbCommand command = connection.CreateCommand();
+            string query = "INSERT INTO users (username, password) VALUES (@username, @password)";
+            command.CommandText = query;
+
+            // parameters
+            //IDFactory factory = new IDFactory();
+            //string id = factory.generateID();
+            //Console.WriteLine($"username = {user.Username}, password = {user.Password} ");
+            //AddParameterWithValue(command, "@id", DbType.String, id);
+            AddParameterWithValue(command, "@username", DbType.String, user.Username);
+            AddParameterWithValue(command, "@password", DbType.String, user.Password);
+
+
+
+
+            command.ExecuteNonQuery();
+        }
     }
+    
 }
